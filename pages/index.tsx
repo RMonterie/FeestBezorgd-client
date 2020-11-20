f5de456274afe1e23fe24aeb932a049fcf6bbe15
roberto <rob.monterie@gmail.com>
2020-11-20 13:54:40 +0100

    Added hero image to the landing page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import BaseLayout from "../layout/BaseLayout";
 
+import "./LandingPage.scss";
+
 export default function Home() {
   return (
     <BaseLayout title="Home">
-      <div className="landing-container">
-        <div>
-          <h1>Feestbezorgd Next.js app</h1>
-          <p>Here is the first section</p>
+      <div id="landing-container">
+        <div className="hero-image">
+          <h1>Feestbezorgd</h1>
+          <h3>Food for your party when you want it.</h3>
         </div>
-        <div>
+        <div id="page-content">
           <p>Here is the second section</p>
         </div>
       </div>
