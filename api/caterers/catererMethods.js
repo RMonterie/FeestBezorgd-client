6a1a682c9d55e3f3998ab6bec5863df3bca48522
Sanjiv <Sanjivjankie@gmail.com>
2020-11-28 04:41:44 +0100

    added axios request for add product

--- api/caterers/catererMethods.js
+++ api/caterers/catererMethods.js
@@ -19,3 +19,22 @@ export const getAllCaterers = async () => {
 
   return;
 };
+
+export const addProductToCatalogue = async (name, price) => {
+  const token = localStorage.getItem("jwt");
+
+  if (token !== null) {
+    const response = await axios.post(`${baseUrl}/caterers/products`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      catererUsername: "admin",
+      name,
+      price,
+    });
+
+    if (response !== null) {
+      return response;
+    }
+  }
+};
--- components/ProductForm/ProductForm.tsx
+++ components/ProductForm/ProductForm.tsx
@@ -1,16 +1,41 @@
 import "./ProductForm.scss";
 
+import axios from "axios";
+
+import { baseUrl } from "../../constants";
+import { addProductToCatalogue } from "../../api/caterers/catererMethods";
+
 const ProductForm = () => {
-  const onSubmitHandler = () => {};
+  var name;
+  var price;
+
+  const onSubmitHandler = async () => {
+    addProductToCatalogue(name, price);
+  };
 
   return (
     <div>
-      <form onSubmit={onSubmitHandler}></form>
-      <label htmlFor="productName">Product Name</label>
-      <input type="text" name="productName" />
+      <form onSubmit={onSubmitHandler}>
+        <label htmlFor="productName">Product Name</label>
+        <input
+          type="text"
+          name="productName"
+          onChange={(event) => {
+            name = event.target.value;
+          }}
+        />
 
-      <label htmlFor="productPrice">Product Price</label>
-      <input type="integer" name="productPrice" />
+        <label htmlFor="productPrice">Product Price</label>
+        <input
+          type="integer"
+          name="productPrice"
+          onChange={(event) => {
+            price = event.target.value;
+          }}
+        />
+
+        <button type="submit">Add product</button>
+      </form>
     </div>
   );
 };
