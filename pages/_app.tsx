9c6980cd8f761b1e912ef38c47cf088f7b872b32
roberto <rob.monterie@gmail.com>
2021-01-13 13:55:24 +0100

    Installed redux-persist package. App now persists redux state on refresh.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import { wrapper } from "../redux/store";
+import { useStore } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const store = useStore((state) => state);
+  return (
+    <PersistGate persistor={store.__persistor} loading={<div>loading...</div>}>
+      <Component {...pageProps} />
+    </PersistGate>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
